8b9b5db6f506c51b30067fc8cc8ccebc064a8bf5
Pushpendra766 <pushpendrachandravanshi>
2023-10-21 23:12:49 +0530

    product page static part done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HomePage from "@/components/HomePage";
 import Navbar from "@/components/Navbar";
+import ProductPage from "@/components/ProductPage/ProductPage";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <ProductPage />
     </div>
   );
 }
