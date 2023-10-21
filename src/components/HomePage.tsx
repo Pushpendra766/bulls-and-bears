e4fd158d62870fbaebd0689d9d38ac61a1a09a54
Pushpendra766 <pushpendrachandravanshi>
2023-10-21 13:03:43 +0530

    initiated data fetching from API

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,10 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import StockCard from "./StockCard";
-
+import axios from "axios";
 const HomePage = () => {
   const [current, setCurrent] = useState("gainer");
+  const [topGainers, setTopGainers] = useState([]);
+  const [topLosers, setTopLosers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const res = await axios.get(
+          "https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=demo"
+        );
+        console.log(res.data);
+        setTopGainers(res.data.top_gainers);
+        setTopLosers(res.data.top_losers);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <div className="mx-10 lg:mx-20 xl:mx-40 mt-5">
       <div className="flex gap-6">
@@ -26,26 +51,15 @@ const HomePage = () => {
         </button>
       </div>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  mt-10">
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
-        <StockCard />
+        {loading
+          ? "Loading..."
+          : current === "gainer"
+          ? topGainers.map((stock, idx) => {
+              return <StockCard data={stock} key={idx} />;
+            })
+          : topLosers.map((stock, idx) => {
+              return <StockCard data={stock} key={idx} />;
+            })}
       </div>
       <div className="w-40 mx-auto my-10">
         <button>Load More</button>
--- src/components/StockCard.tsx
+++ src/components/StockCard.tsx
@@ -1,22 +1,42 @@
 import Image from "next/image";
 
-export default function StockCard() {
+type ArrayObjectType = {
+  ticker: string;
+  price: string;
+  change_amount: string;
+};
+
+type StockCardProps = {
+  data: ArrayObjectType;
+};
+
+const StockCard: React.FC<StockCardProps> = ({ data }) => {
+  const isGainer = parseFloat(data.change_amount) > 0;
   return (
-    <div className="flex gap-8 border rounded-md p-4">
-      {" "}
-      <Image
-        className="rounded-full py-3"
-        src="/google.png"
-        height={70}
-        width={70}
-        alt="Stock icon"
-      />
+    <div className="flex gap-8 border rounded-md p-4 cursor-pointer">
+      <div className="bg-[#fff000] flex justify-center items-center px-7 rounded-full">
+        <h1 className="text-xl font-bold">{data.ticker[0]}</h1>
+      </div>
       <div>
-        <h2 className="font-semibold">Google</h2>
-        <p className="text-slate-500">GOOGL</p>
-        <p>$139.72 </p>
-        <p className="text-green-600 text-sm font-semibold">+45.72</p>{" "}
+        <h2 className="font-semibold">{data.ticker}</h2>
+        <p>${data.price} </p>
+        <p
+          className={`flex gap-2 ${
+            isGainer ? "text-green-600" : "text-red-600"
+          } text-sm font-semibold`}
+        >
+          {isGainer && "+"}
+          {data.change_amount}
+          <Image
+            src={isGainer ? "up_arrow.svg" : "down_arrow.svg"}
+            height={15}
+            width={15}
+            alt="arrow up"
+          />
+        </p>{" "}
       </div>
     </div>
   );
-}
+};
+
+export default StockCard;
