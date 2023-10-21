734759ba8bc5cfc6b358994b6bcce8bf181d7966
Pushpendra766 <pushpendrachandravanshi>
2023-10-21 10:54:58 +0530

    navbar responsiveness done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,32 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between gap-20 px-40 py-5">
-      <span className="flex gap-4">
-        <Image src="/bullsandbears.png" width={40} height={40} alt="Bulls & Bears Icon" />
-        <h1 className="text-xl whitespace-nowrap pt-2 font-bold">
+    <div className="flex gap-6 md:gap-10 lg:gap-20 px-4 md:px-10 lg:px-20 xl:px-40 py-5 border-b">
+      <div className="flex gap-2 lg:gap-4">
+        <Image
+          src="/bullsandbears.png"
+          width={40}
+          height={40}
+          alt="Bulls & Bears Icon"
+        />
+        <h1 className="hidden lg:flex text-xl pt-2 font-bold whitespace-nowrap">
           Bulls & Bears
         </h1>
-      </span>
+        <h1 className="lg:hidden text-xl whitespace-nowrap pt-2 font-bold">
+          B&B
+        </h1>
+      </div>
+      <div></div>
       <div className="flex gap-2 border w-full px-4 rounded-full">
         <Image src="/search.svg" width={15} height={15} alt="search" />
-        <input type="text" placeholder="What are you looking for today ?" className="w-full focus:outline-none"/>
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full focus:outline-none"
+        />
       </div>
-
       <button className="bg-[#048000] px-4 font-semibold text-white rounded-full">
-        Login/Register
+        Th
       </button>
     </div>
   );
